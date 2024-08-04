a0636a3dd8a444d86edbd8c6ca4c18ae37c6adfb
roger_front <shroger1994@gmail.com>
2024-08-04 11:14:41 +0800

    feat: 樣式調整

--- BanThisJandaner.user.js
+++ BanThisJandaner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         勞資不想看到你個sb
 // @namespace    http://tampermonkey.net/
-// @version      1.20
+// @version      1.21
 // @description  通過網頁操作, 達成屏蔽與解除屏蔽使用者
 // @author       You
 // @match        *://jandan.net/*
@@ -149,9 +149,9 @@
     // 生成list按鈕Dom
     var counter = Object.keys(banCode).length
     var listDom = `
-            <div class="ban-list" style="display: flex; justify-content: flex-end; position: fixed; top: 84px; left: 50%; width: 1184px; max-height: calc(100% - 300px); transform: translateX(-50%); pointer-events: none">
-                <a class="toggleList" style="padding: 5px; position: relative; text-align: center; border-radius: 5px; background: #bababa; color: white; font-weight: bold; pointer-events: auto;">屏蔽列表 <span style="display: inline-block; width: 20px; height: 20px">+</span><span style="position: absolute; width: 20px; height: 20px; top: 5px; right: 5px; background: #bababa; display: none;">-</span></a>
-                <ul style="margin-bottom: 3px; position: absolute; width: 90px; top: calc(100% + 10px); display: none; color: gray; overflow: hidden;">
+            <div class="ban-list" style="display: flex; flex-wrap: wrap; justify-content: flex-end; position: fixed; top: 84px; width: 1184px; pointer-events: none; width: calc((100vw - 984px - 60px) / 2); max-height: calc(100% - 300px); box-sizing: border-box; left: calc(100% -(100vw - 984px - 30px) / 2);">
+                <a class="toggleList" style="padding: 5px; position: relative; text-align: center; width: 100%; border-radius: 5px; background: #bababa; color: white; font-weight: bold; pointer-events: auto;">屏蔽列表 <span style="display: inline-block; width: 20px; height: 20px">+</span><span style="position: absolute; width: 20px; height: 20px; top: 5px; right: 5px; background: #bababa; display: none;">-</span></a>
+                <ul style="margin-bottom: 3px; position: relative; width: 90px; top: calc(100% + 10px); display: none; width: 100%; color: gray; overflow: auto; height: calc(100vh - 196px);">
     `
 
     // 循环遍历 banCode 对象中的已屏蔽用户
