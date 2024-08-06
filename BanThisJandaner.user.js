6ff13cc1c125f0236d83e4dd6d8144256efaca44
roger_front <shroger1994@gmail.com>
2024-08-06 17:53:03 +0800

    feat: 解衝突

--- BanThisJandaner.user.js
+++ BanThisJandaner.user.js
@@ -1,11 +1,7 @@
 // ==UserScript==
 // @name         勞資不想看到你個sb
 // @namespace    http://tampermonkey.net/
-<<<<<<< Updated upstream
-// @version      1.24
-=======
 // @version      1.26
->>>>>>> Stashed changes
 // @description  通過網頁操作, 達成屏蔽與解除屏蔽使用者
 // @author       You
 // @match        *://jandan.net/*
